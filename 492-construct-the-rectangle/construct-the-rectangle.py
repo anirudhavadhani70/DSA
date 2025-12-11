eeb919311639e7aff41d4804469ec4ffb34e6942
ANIRUDH AVADHANI <65844496+anirudhavadhani70@users.noreply.github.com>
2025-12-10 19:57:50 -0600

    Time: 4 ms (22.87%) | Memory: 17.8 MB (62.42%) - LeetSync

---
+++ 492-construct-the-rectangle/construct-the-rectangle.py
@@ -0,0 +1,9 @@
+from math import sqrt
+class Solution:
+    def constructRectangle(self, area: int) -> List[int]:
+        w=int(sqrt(area))
+        while area%w:
+            w-=1
+        return[area//w,w]
+        
+        
